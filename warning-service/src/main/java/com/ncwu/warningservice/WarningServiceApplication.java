5759fb54278a45c07e08868f8ed9db50b9447edd
jingxu-i-xiaozhou <jingxushi13@gmail.com>
2025-12-21 17:02:24 +0800

    完成基础框架和依赖项

---
+++ warning-service/src/main/java/com/ncwu/warningservice/WarningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ncwu.warningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarningServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarningServiceApplication.class, args);
+    }
+
+}
---
+++ warning-service/src/test/java/com/ncwu/warningservice/WarningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncwu.warningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

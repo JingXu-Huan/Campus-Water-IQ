c25487490d4b859f0363d8e26a44d9907120e7e2
jingxu-i-xiaozhou <jingxushi13@gmail.com>
2026-01-01 17:49:33 +0800

    新增异常类

--- IoT-device/src/main/java/com/ncwu/iotdevice/exception/MessageSendException.java
+++ IoT-device/src/main/java/com/ncwu/iotdevice/exception/MessageSendException.java
@@ -10,4 +10,7 @@ public class MessageSendException extends RuntimeException {
     public MessageSendException(String message) {
         super(message);
     }
+    public MessageSendException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }

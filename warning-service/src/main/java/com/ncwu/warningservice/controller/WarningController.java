f702be57ac1458cb9ef768ebe55916e1c6f8baee
jingxu <jingxushi13@gmail.com>
2026-04-02 18:04:32 +0800

    修复邮件验证逻辑，新增警告控制器

---
+++ warning-service/src/main/java/com/ncwu/warningservice/controller/WarningController.java
@@ -0,0 +1,19 @@
+package com.ncwu.warningservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jingxu
+ * @version 1.0.0
+ * @since 2026/4/2
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/warning")
+public class WarningController {
+
+}
